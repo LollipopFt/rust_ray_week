f6e38e233b7db42fa1b4e1f54937b2901ee5aea4
LollipopFt <lollipopz1426@gmail.com>
2022-02-09 21:17:16 +0800

    3.2

--- src/vec3.rs
+++ src/vec3.rs
@@ -1,4 +1,4 @@
-use std::ops::{AddAssign, DivAssign, MulAssign, Neg};
+use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Sub};
 
 #[derive(Default)]
 pub struct Vec3(pub f64, pub f64, pub f64);
@@ -20,21 +20,40 @@ impl Vec3 {
     pub fn length_squared(&self) -> f64 {
         self.0*self.0 + self.1*self.1 + self.2*self.2
     }
+
+    pub fn dot(&self, other: Vec3) -> f64 {
+            self.0*other.0
+            + self.1*other.1
+            + self.2*other.2
+    }
+    pub fn cross(&self, other: Vec3) -> Vec3 {
+        Vec3(
+            self.1*other.2 - self.2*other.1,
+            self.2*other.0 - self.0*other.2,
+            self.0*other.1 - self.1*other.0
+        )
+    }
+    pub fn unit_vector(self) -> Vec3 {
+        let length = self.length();
+        Vec3(
+            self.0/length,
+            self.1/length,
+            self.2/length
+        )
+    }
 }
 
 impl Neg for Vec3 {
     type Output = Self;
-    fn neg(self) -> Self::Output {
+    fn neg(self) -> Self {
         Vec3(-self.0, -self.1, -self.2)
     }
 }
 impl AddAssign for Vec3 {
     fn add_assign(&mut self, other: Self) {
-        *self = Self(
-            self.0 + other.0,
-            self.1 + other.1,
-            self.2 + other.2
-        );
+            self.0 += other.0;
+            self.1 += other.1;
+            self.2 += other.2;
     }
 }
 impl MulAssign<f64> for Vec3 {
@@ -52,3 +71,42 @@ impl DivAssign<f64> for Vec3 {
     }
 }
 
+impl Add for Vec3 {
+    type Output = Self;
+    fn add(self, other: Self) -> Self {
+        Self(self.0 + other.0, self.1 + other.1, self.2 + self.2)
+    }
+}
+impl Sub for Vec3 {
+    type Output = Self;
+    fn sub(self, other: Self) -> Self {
+        Self(self.0 - other.0, self.1 - other.1, self.2 - other.2)
+    }
+}
+impl Mul for Vec3 {
+    type Output = Self;
+    fn mul(self, rhs: Self) -> Self {
+        Self(self.0*rhs.0, self.1*rhs.1, self.2*rhs.2)
+    }
+}
+impl Mul<Vec3> for f64 {
+    type Output = Vec3;
+    fn mul(self, rhs: Vec3) -> Vec3 {
+        Vec3(self*rhs.0, self*rhs.1, self*rhs.2)
+    }
+}
+impl Mul<f64> for Vec3 {
+    type Output = Self;
+    fn mul(self, rhs: f64) -> Self {
+        Self(self.0*rhs, self.1*rhs, self.2*rhs)
+    }
+}
+impl Div<f64> for Vec3 {
+    type Output = Self;
+    fn div(self, rhs: f64) -> Self {
+        if rhs == 0.0 {
+            panic!("Cannot divide by zero-valued `Rational`!");
+        }
+        Self(self.0/rhs, self.1/rhs, self.2/rhs)
+    }
+}

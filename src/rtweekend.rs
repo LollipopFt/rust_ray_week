ce2275f8ef3e933002665fd34bb083e4a31cb651
LollipopFt <lollipopz1426@gmail.com>
2022-02-13 16:09:40 +0800

    7.1

--- src/rtweekend.rs
+++ src/rtweekend.rs
@@ -1,3 +1,13 @@
+use rand::{Rng, thread_rng};
+
 fn degree_toradian(degree: f64) -> f64 {
     degree*std::f64::consts::PI / 180.0
+}
+
+fn random() -> f64 {
+    thread_rng().gen::<f64>()
+}
+
+fn rand_double(min: f64, max: f64) -> f64 {
+    min + (max-min)*random()
 }
